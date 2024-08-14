81b5c6dd4a2dd66b1a9136b911086d9c10392ce3
A-AnilKumar <anilkumar.akkaraveni@gmail.com>
2024-08-14 11:43:39 +0530

    Calculator with BigInteger added

---
+++ src/com/calculator/Calculator.java
@@ -0,0 +1,77 @@
+package com.calculator;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Calculator {
+
+    public void calculate(){
+
+        String number1 ;
+        String number2 ;
+
+//    BigInteger bigInteger = new BigInteger();
+
+        Scanner scanner = new Scanner(System.in);
+//        boolean performOperation = true;
+        while(true){
+            displayMenu();
+            int choice = scanner.nextInt();
+            if (choice == 5){
+                System.out.println("You are done ..!!");
+                break;
+            }
+
+            System.out.println("Enter number1");
+            number1 =  scanner.next();
+            BigInteger num1 = new BigInteger(number1);
+
+            System.out.println("Enter number2");
+            number2 = scanner.next();
+            BigInteger num2 = new BigInteger(number2);
+
+//            BigInteger result = new BigInteger("0");
+
+            switch (choice){
+                case 1 -> {
+//                    result = num1.add(num2);
+                    System.out.println("Result : "+num1.add(num2));
+
+                }
+                case 2 ->{
+//                    result = num1.subtract(num2);
+                    System.out.println("Result : "+num1.subtract(num2));
+                }
+                case 3 -> {
+//                    result = num1.multiply(num2);
+                    System.out.println("Result : "+num1.multiply(num2));
+                }
+                case 4 ->{
+                    if (num2.equals(new BigInteger("0")) ){
+                        System.out.println("Cannot divide by zero");
+                    }
+                    else {
+//                        result = num1.divide(num2);
+                        System.out.println("Result : "+num1.divide(num2));
+                    }
+                }
+                default -> {
+                    System.out.println("Pick an option from ( 1 - 4 )");
+                }
+
+            }
+        }
+        scanner.close();
+    }
+
+    private void displayMenu() {
+        System.out.println("-----------------------------");
+        System.out.println("Pick a option to perform");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. End");
+    }
+
+}
