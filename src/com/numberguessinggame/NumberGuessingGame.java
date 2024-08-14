73a5fd29e332660f82838efa497844322294d3b2
A-AnilKumar <anilkumar.akkaraveni@gmail.com>
2024-08-14 11:45:16 +0530

    Number guessing game file added

---
+++ src/com/numberguessinggame/NumberGuessingGame.java
@@ -0,0 +1,34 @@
+package com.numberguessinggame;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public void guessNumber(){
+        Scanner scanner = new Scanner(System.in);
+//        int userInput = 0;
+        Random random = new Random();
+        int randomNumber = random.nextInt(1000);
+
+        while(true){
+            System.out.println("Guess the Number");
+            int userInput = scanner.nextInt();
+
+            if (userInput == randomNumber){
+                System.out.println("You guessed it ..!!");
+                break;
+            }
+            else if(userInput > randomNumber){
+                System.out.println("Number is HIGH");
+                System.out.println("Try again...");
+            }
+            else {
+                System.out.println("Number is LOW");
+                System.out.println("Try again...");
+            }
+        }
+        scanner.close();
+    }
+
+}
