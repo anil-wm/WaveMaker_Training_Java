9a2c15a5a6b4d4d7b7d085f96dcf26740c43eee6
anil-wm <anilkumar.akkaraveni@wavemaker.com>
2024-12-06 11:12:04 +0530

    commented lines that needs user input

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import com.numberguessinggame.NumberGuessingGame;
 
 public class Main {
     public static void main(String[] args) {
-        NumberGuessingGame numberGuessingGame = new NumberGuessingGame();
-        numberGuessingGame.guessNumber();
-
-        Calculator calculator = new Calculator();
-        calculator.calculate();
+//        NumberGuessingGame numberGuessingGame = new NumberGuessingGame();
+  //      numberGuessingGame.guessNumber();
+//
+    //    Calculator calculator = new Calculator();
+      //  calculator.calculate();
 
         DataTypes dataTypes = new DataTypes();
         dataTypes.printAllDataTypes();
